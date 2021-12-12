ec6e2ce8b3e2b9a816a4d5c73d35f3243ee7d28b
Darryl <green.jr.darryl@gmail.com>
2021-12-12 09:47:02 -0500

    updating repor
    adding break, copy, and for exercises

---
+++ copy.py
@@ -0,0 +1,6 @@
+msg = input("Say Something: ")
+
+while msg != "Stop copying me": 
+    print(msg)
+    msg = input()
+print("UGH FINE YOU BIG BABY!")
---
+++ for.py
@@ -0,0 +1,11 @@
+# for x in range(1,10):
+# 	print(x)
+# 	print(x*x)
+
+for letter in "coffee":
+	print(letter*10)
+
+	numbers = [1,2,3,4]
+
+for number in numbers:
+	print(number)
