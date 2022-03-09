b8ad653e35837044d689bcea40bca5ba34e13149
Darryl <green.jr.darryl@gmail.com>
2022-03-08 19:16:47 -0500

    updating practice exercises

---
+++ list_comprehensions.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6]
+
+print(numbers)
+
+print([num*2 for num in numbers])
+
+name = 'darryl'
+
+print([char.upper() for char in name])
+
+friends = ['aoun', 'john', 'chuck', 'nyck', 'steve', 'evan', 'chamie']
+
+print([friends[0].upper() for friend in friends])
+
+print([num*10 for num in range(1,6)])
+
+print([bool(val) for val in [0, [], '']])
---
+++ list_methods.py
@@ -0,0 +1,8 @@
+cars = ['Ford', 'BMW', 'Volvo', 'GMC', 'Chrysler', 'Mazda']
+
+print(cars)
+
+cars.sort()
+
+print('Sorted list: ') 
+print(cars)
