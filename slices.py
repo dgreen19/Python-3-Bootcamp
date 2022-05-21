94083ecf782ad7279e931e7314495b922ff52783
Darryl <green.jr.darryl@gmail.com>
2022-05-21 10:03:16 -0400

    Update slices.py

--- slices.py
+++ slices.py
@@ -3,3 +3,35 @@ print (first_list)
 print ("Numbers cut from list:") 
 print (first_list [1:3])
 
+
+
+f = lambda n:1 if n <= 1 else n * f(n-1)
+g = f(4)
+print(g)
+
+words = ['Hello', 'World']
+
+for i, word in enumerate(words):
+    word.lower()
+    words[i] = word[::-1]
+
+print(words)
+
+nums = [0, -1, -2]
+
+def has_pos_neg(nums):
+    has_pos = False
+    has_neg = False
+    for num in nums:
+        has_pos = num > 0
+        has_neg = num < 0
+    return (has_pos, has_neg)
+
+def _(func, items):
+    i = 0
+    for item in items:
+        if func(item):
+            items[i] = item
+            i += 1
+    del items[i:]
+
