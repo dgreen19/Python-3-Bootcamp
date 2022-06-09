b3ccf6608c25612443b7c6c90e1e18584c5293f1
Darryl <green.jr.darryl@gmail.com>
2022-06-09 13:40:02 -0400

    Update image_downloader2.py

--- image_downloader2.py
+++ image_downloader2.py
@@ -4,29 +4,19 @@ import numpy as np
 import pylab as plt
 
 
-csv = pd.read_csv("/users/dgreen/Downloads/sianctapi-selected-observations-62852856d9811.csv")
+csv = pd.read_csv("/users/dgreen/Downloads/sianctapi-selected-observations-629f614f1b157.csv")
 for id in csv["Sequence ID"]:
     if id[0] == "d":
         try:
-            for number in range(0,1000):
+            for number in range(len(id)):
                 url = f"http://ids.si.edu/ids/deliveryService?id=emammal_image_{id}i{number}&max=1000"
                 ret = requests.get(url)
                 if ret.ok:
                     content = ret.content
-                    file_handle = open(f"/Users/dgreen/Docs/test_images2/{id}_{number}.png","wb")
+                    file_handle = open(f"/Users/dgreen/Docs/coyote_images/{id}_{number}.png","wb")
                     file_handle.write(content)
                     file_handle.close()
-                    print("The image was successfully saved.")
+                    print(f"The image {url} was successfully saved.")
         except:
-            print("The URL " + url + " does not work.")
-        finally:
-            for number in range(0,100):
-                url = f"http://ids.si.edu/ids/deliveryService?id=emammal_image_{id}i{number}&max=1000"
-                ret = requests.get(url)
-                if ret.ok:
-                    content = ret.content
-                    file_handle = open(f"/Users/dgreen/Docs/test_images2/{id}_{number}.png","wb")
-                    file_handle.write(content)
-                    file_handle.close()
-                    print("The image was successfully saved.")
+            print(f"The URL {url} does not work.")
 print("Done")
