52c10f5b8358b589f6963906c1e14044930d0a43
Darryl <green.jr.darryl@gmail.com>
2022-08-23 13:17:07 -0400

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,42 @@
+
+"""ARRAYS NEED TO BE ALREADY SORTED BEFORE MERGE!"""
+def mergeArrays(arr1, arr2) -> list:
+    results = []
+    # Starting pointer in Array 1
+    i = 0
+    # Starting pointer in Array 2
+    j = 0
+    while i < len(arr1) and j < len(arr2):
+        if(arr2[j] >= arr1[i]):
+            results.append(arr1[i])
+            i += 1
+        else:
+            results.append(arr2[j])
+            j+=1
+    while i < len(arr1): 
+        results.append(arr1[i])
+        print(arr1[i])
+        i+=1
+        
+            
+    while j < len(arr2):
+        results.append(arr2[j])
+        print(arr2[j])
+        j+=1
+        
+        
+    print(arr1)
+    print(arr2)
+    return results
+
+print(mergeArrays([100,200], [1,2,3,5,6]))
+
+mergedArrays = mergeArrays()
+
+def mergeSort(mergedArrays: list) -> list:
+    if len(mergedArrays) <= 1:
+        return mergedArrays
+     mid = round(len(mergedArrays)/2)
+     left = mergedArrays.slice(0,mid)
+     right
+    
